32cd611ee1a4e91756316d608de7e1027fdc1b4c
psmietanaa <piotrsmietana1998@gmail.com>
2020-03-15 13:31:52 -0500

    Added footers and more templates

--- run.py
+++ run.py
@@ -5,7 +5,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title='Home')
+
+@app.route("/about-us")
+def aboutUs():
+    return render_template("about-us.html", title='About Us')
+
+@app.route("/contact-us")
+def contactUs():
+    data = {
+        "phone_number": "319-834-0276",
+        "email": "hawkair2020@gmail.com"
+    }
+    return render_template("contact-us.html", title='Contact Us', data=data)
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title='Login / Sign Up')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='127.0.0.1', port=5000)
