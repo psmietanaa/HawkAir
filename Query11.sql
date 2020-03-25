397061cb15836a01453ae7e86513d0bcc0c89e47
psmietanaa <piotrsmietana1998@gmail.com>
2020-03-25 15:01:16 -0500

    Query11

---
+++ Query11.sql
@@ -0,0 +1,19 @@
+INSERT INTO `HawkAir`.`Flights` VALUES
+('AA5144','Airbus 319','ORD','DEN','14:57','1:52','On time',240,160,0,0);
+
+INSERT INTO `HawkAir`.`Schedule` VALUES
+(1,1,1,1,1,1,1,'AA5144');
+
+INSERT INTO `HawkAir`.`Flights` VALUES
+('AA8623','Airbus 319','ORD','DEN','19:25','1:52','On time',280,150,0,0);
+
+INSERT INTO `HawkAir`.`Schedule` VALUES
+(1,1,1,1,1,1,1,'AA8623');
+
+---------------------------------------------------------------------------------
+
+INSERT INTO `HawkAir`.`Flights` VALUES
+('AA3287','Airbus 319','DEN','ORD','10:30','1:52','On time',195,235,0,0);
+
+INSERT INTO `HawkAir`.`Schedule` VALUES
+(1,1,1,1,1,1,1,'AA3287');
