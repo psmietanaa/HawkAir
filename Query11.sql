bf7906dc90e72baf5b9157cccf5ce72635d34a9a
Sriram <43755762+srinvsn@users.noreply.github.com>
2020-03-25 17:53:54 -0500

    Update Query11.sql

--- Query11.sql
+++ Query11.sql
@@ -17,3 +17,22 @@ INSERT INTO `HawkAir`.`Flights` VALUES
 
 INSERT INTO `HawkAir`.`Schedule` VALUES
 (1,1,1,1,1,1,1,'AA3287');
+
+
+--------------------------------------------------------------------------------------
+
+DELIMITER //
+CREATE PROCEDURE ConnectingFlights(IN fromCity VARCHAR(3), IN toCity VARCHAR(3), IN weekDay VARCHAR(9))
+BEGIN
+SELECT Dept1 = Flights.fromCity, Arr2 = flights.toCity, Arr1 = Dept2 = Flights.From = (Select RAND() * Flights.From from Flights != fromCity AND TIMEDIFF('ArrivalTime.fromCity', 'DepartTime.Dept2'  = 1:30:00) 
+FROM Flights
+END //
+DELIMITER ;
+
+" I have written this query with the following logic in my mind -
+         Dept1 (FromCity Location) -------> Arr1 (Intermediate Location)
+         Dept2 (Intermediate Location same as Arr1) -----------> Arr2 (ToCity location which is the final destination)
+  So the Arr1 (Intermediate Location) has to be selected in a way that Both Arr1 and Dept2 are the same and they are not equal to Dept1.
+  Also, The time difference between ArrivalTime of Dept1 Flight - DepartTime of Dept2 Flight should be 1.5 Hrs(which can be modified)
+  For this we would need to have a track of ArrivalTime of each flight just like how we are having the DepartTime."
+                                                                                       
