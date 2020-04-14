1c201fe42e5cdb4211059d3a6baa492cb1911f0a
psmietanaa <piotrsmietana1998@gmail.com>
2020-04-14 13:25:06 -0500

    Design 2.0

--- Procedures.sql
+++ Procedures.sql
@@ -1,44 +1,52 @@
 DELIMITER //
 CREATE PROCEDURE SearchFlights(IN fromCity VARCHAR(3), IN toCity VARCHAR(3), IN weekDay VARCHAR(9))
 BEGIN
-    SET @sql = CONCAT('SELECT flights.FlightID FROM flights, schedule WHERE flights.FlightID = schedule.FlightID AND flights.From = ''', fromCity, ''' AND flights.To = ''', toCity, ''' AND schedule.', weekDay, ' = 1 ORDER BY flights.DepartTime;');
+    SET @sql = CONCAT('SELECT flights.FlightID FROM flights, routes, schedules WHERE flights.RouteID = routes.RouteID AND flights.ScheduleID = schedules.ScheduleID AND routes.From = ''', fromCity, ''' AND routes.To = ''', toCity, ''' AND schedules.', weekDay, ' = 1 ORDER BY flights.DepartTime;');
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 END //
 DELIMITER ;
 
+# CALL SearchFlights('ORD', 'LAX', 'Monday')
+
 DELIMITER //
 CREATE PROCEDURE CheckSeats(IN flightNumber VARCHAR(6), IN fDate DATE, IN passengers INT)
 BEGIN
-    SELECT flights.From, flights.To, flights.DepartTime, flights.FlightID, flights.AircraftID, flights.Duration,
-        aircrafts.TotalEconomySeats - (SELECT COUNT(*) FROM bookings WHERE FlightID = flightNumber AND FlightDate = fDate AND Class = 'Economy') AS AvailableEconomySeats,
-        aircrafts.TotalFirstClassSeats - (SELECT COUNT(*) FROM bookings WHERE FlightID = flightNumber AND FlightDate = fDate AND Class = 'First Class') AS AvailableFirstClassSeats
-    FROM aircrafts, flights
-    WHERE aircrafts.AircraftID = flights.AircraftID AND flights.FlightID = flightNumber AND
-        (aircrafts.TotalEconomySeats - (SELECT COUNT(*) FROM bookings WHERE FlightID = flightNumber AND FlightDate = fDate AND Class = 'Economy') >= passengers OR
-        aircrafts.TotalFirstClassSeats - (SELECT COUNT(*) FROM bookings WHERE FlightID = flightNumber AND FlightDate = fDate AND Class = 'First Class') >= passengers);
+    SELECT routes.From, routes.To, flights.DepartTime, flights.FlightID, flights.AircraftID, routes.Duration,
+        aircrafts.EconomySeats - (SELECT COUNT(*) FROM multiplebookings WHERE multiplebookings.FlightID = flightNumber AND FlightDate = fDate AND Class = 'Economy') AS AvailableEconomySeats,
+        aircrafts.FirstClassSeats - (SELECT COUNT(*) FROM multiplebookings WHERE multiplebookings.FlightID = flightNumber AND FlightDate = fDate AND Class = 'First Class') AS AvailableFirstClassSeats
+    FROM aircrafts, flights, routes
+    WHERE aircrafts.AircraftID = flights.AircraftID AND flights.RouteID = routes.RouteID AND flights.FlightID = flightNumber AND
+        (aircrafts.EconomySeats - (SELECT COUNT(*) FROM multiplebookings WHERE multiplebookings.FlightID = flightNumber AND FlightDate = fDate AND Class = 'Economy') >= passengers OR
+        aircrafts.FirstClassSeats - (SELECT COUNT(*) FROM multiplebookings WHERE multiplebookings.FlightID = flightNumber AND FlightDate = fDate AND Class = 'First Class') >= passengers);
 END //
 DELIMITER ;
+
+# CALL CheckSeats('AA2470', '2020-03-24', 5)
 
 DELIMITER //
 CREATE PROCEDURE GetFare(IN flightNumber VARCHAR(6))
 BEGIN
-    SELECT PriceEconomy, PriceFirstClass
-    FROM flights
-    WHERE FlightID = flightNumber;
+    SELECT fares.PriceEconomy, fares.PriceFirstClass
+    FROM fares, flights
+    WHERE fares.FareID = flights.FareID AND flights.FlightID = flightNumber;
 END //
 DELIMITER ;
+
+# CALL GetFare('AA2470')
 
 DELIMITER //
 CREATE PROCEDURE GetSeats(IN flightNumber VARCHAR(6), IN fDate DATE)
 BEGIN
-    SELECT aircrafts.TotalEconomySeats - (SELECT COUNT(*) FROM bookings WHERE FlightID = flightNumber AND FlightDate = fDate AND Class = 'Economy') AS AvailableEconomySeats, 
-            aircrafts.TotalFirstClassSeats - (SELECT COUNT(*) FROM bookings WHERE FlightID = flightNumber AND FlightDate = fDate AND Class = 'First Class') AS AvailableFirstClassSeats
+    SELECT aircrafts.EconomySeats - (SELECT COUNT(*) FROM multiplebookings WHERE multiplebookings.FlightID = flightNumber AND FlightDate = fDate AND Class = 'Economy') AS AvailableEconomySeats, 
+        aircrafts.FirstClassSeats - (SELECT COUNT(*) FROM multiplebookings WHERE multiplebookings.FlightID = flightNumber AND FlightDate = fDate AND Class = 'First Class') AS AvailableFirstClassSeats
     FROM aircrafts, flights
     WHERE aircrafts.AircraftID = flights.AircraftID AND flights.FlightID = flightNumber;
 END //
 DELIMITER ;
+
+# CALL GetSeats('AA2470', '2020-03-24')
 
 DELIMITER //
 CREATE PROCEDURE GetBookingIDs()
@@ -48,50 +56,72 @@ BEGIN
 END //
 DELIMITER ;
 
+# CALL GetBookingIDs()
+
 DELIMITER //
-CREATE PROCEDURE CreateBooking(IN bookingID VARCHAR(6), IN passenger VARCHAR(100), IN flightDate Date, IN class VARCHAR(20), IN userID VARCHAR(45), IN flightID VARCHAR(6))
+CREATE PROCEDURE CreateBooking(IN bookingID VARCHAR(6), IN userID VARCHAR(45))
 BEGIN
     INSERT INTO bookings VALUES
-    (bookingID, passenger, flightDate, class, NULL, userID, flightID);
+    (bookingID, userID);
 END //
 DELIMITER ;
+
+# CALL CreateBooking('ABCXYZ', 100001)
+
+DELIMITER //
+CREATE PROCEDURE CreateMultipleBookings(IN bookingID VARCHAR(6), IN flightID VARCHAR(6), IN flightDate Date, IN passenger VARCHAR(100), IN class VARCHAR(20))
+BEGIN
+    INSERT INTO multiplebookings VALUES
+    (bookingID, flightID, flightDate, passenger, class, '');
+END //
+DELIMITER ;
+
+# CALL CreateMultipleBookings('ABCXYZ', 'AA2470', '2020-03-24', 'Test', 'First Class')
 
 DELIMITER //
 CREATE PROCEDURE GetBooking(IN bookingNumber VARCHAR(6))
 BEGIN
-    SELECT bookings.BookingID, flights.From, flights.To, bookings.FlightDate, flights.DepartTime, flights.FlightID, flights.Duration, bookings.Class, bookings.Passenger
-    FROM bookings, flights
-    WHERE bookings.FlightID = flights.FlightID and bookings.BookingID = bookingNumber;
+    SELECT multiplebookings.BookingID, routes.From, routes.To, multiplebookings.FlightDate, flights.DepartTime, flights.FlightID, routes.Duration, multiplebookings.Class, multiplebookings.Passenger
+    FROM flights, multiplebookings, routes
+    WHERE multiplebookings.FlightID = flights.FlightID AND flights.RouteID = routes.RouteID AND multiplebookings.BookingID = bookingNumber;
 END //
 DELIMITER ;
+
+# CALL GetBooking('ABCDEF')
 
 DELIMITER //
 CREATE PROCEDURE YourTrip(IN fName VARCHAR(45), IN lName VARCHAR(45), IN bookingNumber VARCHAR(6))
 BEGIN
-    SELECT bookings.BookingID, flights.From, flights.To, bookings.FlightDate, flights.DepartTime, flights.FlightID, flights.Duration, bookings.Class, bookings.Passenger
-    FROM bookings, flights, users
-    WHERE bookings.FlightID = flights.FlightID AND bookings.UserID = users.UserID AND
+    SELECT bookings.BookingID, routes.From, routes.To, multiplebookings.FlightDate, flights.DepartTime, flights.FlightID, routes.Duration, multiplebookings.Class, multiplebookings.Passenger
+    FROM bookings, flights, multiplebookings, routes, users
+    WHERE multiplebookings.FlightID = flights.FlightID AND flights.RouteID = routes.RouteID AND bookings.BookingID = multiplebookings.BookingID AND bookings.UserID = users.UserID AND
         UCASE(users.FirstName) = UCASE(fname) AND UCASE(users.LastName) = UCASE(lname) AND UCASE(bookings.BookingID) = UCASE(bookingNumber);
 END //
 DELIMITER ;
 
+# CALL YourTrip('Roger', 'McCubbin', 'ABCDEF')
+
 DELIMITER //
 CREATE PROCEDURE GetFlightsStatusDate(IN fromCity VARCHAR(3), IN toCity VARCHAR(3), IN fDate Date)
 BEGIN
-    SELECT bookings.BookingID, flights.From, flights.To, bookings.FlightDate, flights.DepartTime, flights.FlightID, flights.Duration, flights.AircraftID, flights.FlightStatus
-    FROM bookings, flights
-    WHERE bookings.FlightID = flights.FlightID AND flights.From = fromCity AND flights.To = toCity AND bookings.FlightDate = fDate ORDER BY flights.DepartTime;
+    SELECT multiplebookings.BookingID, routes.From, routes.To, multiplebookings.FlightDate, flights.DepartTime, flights.FlightID, routes.Duration, flights.AircraftID, flights.FlightStatus
+    FROM multiplebookings, flights, routes
+    WHERE multiplebookings.FlightID = flights.FlightID AND flights.RouteID = routes.RouteID AND routes.From = fromCity AND routes.To = toCity AND multiplebookings.FlightDate = fDate ORDER BY flights.DepartTime;
 END //
 DELIMITER ;
+
+# CALL GetFlightsStatusDate('ORD', 'LAX', '2020-03-24')
 
 DELIMITER //
 CREATE PROCEDURE GetFlightsStatusNumber(IN flightNumber VARCHAR(6), IN fDate Date)
 BEGIN
-    SELECT bookings.BookingID, flights.From, flights.To, bookings.FlightDate, flights.DepartTime, flights.FlightID, flights.Duration, flights.AircraftID, flights.FlightStatus
-    FROM bookings, flights
-    WHERE bookings.FlightID = flights.FlightID AND UCASE(flights.FlightID) = UCASE(flightNumber) AND bookings.FlightDate = fDate ORDER BY flights.DepartTime;
+    SELECT multiplebookings.BookingID, routes.From, routes.To, multiplebookings.FlightDate, flights.DepartTime, flights.FlightID, routes.Duration, flights.AircraftID, flights.FlightStatus
+    FROM multiplebookings, flights, routes
+    WHERE multiplebookings.FlightID = flights.FlightID AND flights.RouteID = routes.RouteID AND UCASE(flights.FlightID) = UCASE(flightNumber) AND multiplebookings.FlightDate = fDate ORDER BY flights.DepartTime;
 END //
 DELIMITER ;
+
+# CALL GetFlightsStatusNumber('AA2470', '2020-03-24')
 
 DELIMITER //
 CREATE PROCEDURE ValidateUser(IN user VARCHAR(45))
@@ -102,6 +132,9 @@ BEGIN
 END //
 DELIMITER ;
 
+# CALL ValidateUser('Quinnow')
+# CALL ValidateUser('100001')
+
 DELIMITER //
 CREATE PROCEDURE ValidateAdmin(IN user VARCHAR(45), IN pass VARCHAR(64))
 BEGIN
@@ -111,71 +144,124 @@ BEGIN
 END //
 DELIMITER ;
 
+# CALL ValidateAdmin('admin', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8')
+
 DELIMITER //
-CREATE PROCEDURE CreateUser(IN Title VARCHAR(6), IN FirstName VARCHAR(45), IN MiddleName VARCHAR(45), IN LastName VARCHAR(45), IN PreferredName VARCHAR(45), IN Sex VARCHAR(6), IN DOB Date, IN Street VARCHAR(100), IN City VARCHAR(45), IN ZipCode VARCHAR(10), IN State VARCHAR(45), IN Country VARCHAR(45), IN Phone VARCHAR(20), IN Email VARCHAR(100), IN Username VARCHAR(45), IN Pass VARCHAR(64), IN SecurityQuestion VARCHAR(100), IN SecurityAnswer VARCHAR(45))
+CREATE PROCEDURE CreateUser(IN Title INT, IN FirstName VARCHAR(45), IN MiddleName VARCHAR(45), IN LastName VARCHAR(45), IN PreferredName VARCHAR(45), IN Sex VARCHAR(6), IN DOB Date, IN Street VARCHAR(100), IN City VARCHAR(45), IN ZipCode VARCHAR(10), IN State VARCHAR(45), IN Country VARCHAR(45), IN Phone VARCHAR(20), IN Email VARCHAR(100), IN Username VARCHAR(45), IN Pass VARCHAR(64), IN SecurityQuestion INT, IN SecurityAnswer VARCHAR(45))
 BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+        BEGIN
+            ROLLBACK;
+        END;
+    COMMIT;
+    
+    START TRANSACTION;
     INSERT INTO users VALUES
-    (NULL, Title, FirstName, MiddleName, LastName, PreferredName, Sex, DOB, Street, City, ZipCode, State, Country, Phone, Email, Username, Pass, SecurityQuestion, SecurityAnswer, 1, 0);
+    (NULL, Title, FirstName, MiddleName, LastName, PreferredName, Sex, DOB, Phone, Email, Username, Pass, SecurityQuestion, SecurityAnswer);
+    INSERT INTO addresses VALUES
+    ((SELECT UserID from users WHERE users.Username = Username), Street, City, ZipCode, State, Country);
+    INSERT INTO hawkadvantage VALUES
+    ((SELECT UserID FROM users WHERE users.Username = Username), 0);
+    COMMIT;
 END //
 DELIMITER ;
+
+# CALL CreateUser(1,'Piotr', 'Romuald', 'Smietana', 'Peter', 'Male', '1998-04-27', 'Kamieniecka 6', 'Krakow', '30389', '', 'Poland', '319-834-0276', 'piotrromuald-smietana@uiowa.edu', 'psmietana', 'hash@hash', 1, 'Tennis')
 
 DELIMITER //
 CREATE PROCEDURE MyInfo(IN user VARCHAR(45))
 BEGIN
-    SELECT *
-    FROM users
-    WHERE Username = user;
+    SELECT users.UserID, titles.Title, users.FirstName, users.MiddleName, users.LastName, users.PreferredName, users.Sex, users.DOB,
+        addresses.Street, addresses.City, addresses.ZipCode, addresses.State, users.Phone, users.Email, users.Username, hawkadvantage.Miles
+    FROM addresses, hawkadvantage, titles, users 
+    WHERE addresses.UserID = users.UserID AND users.TitleID = titles.TitleID AND users.UserID = hawkadvantage.UserID AND users.Username = user;
 END //
 DELIMITER ;
+
+# CALL MyInfo('Quinnow')
 
 DELIMITER //
 CREATE PROCEDURE MyTrips(IN user VARCHAR(45))
 BEGIN
-    SELECT bookings.BookingID, flights.From, flights.To, bookings.FlightDate, flights.DepartTime, flights.FlightID, flights.Duration, bookings.Class, bookings.Passenger
-    FROM bookings, flights, users
-    WHERE bookings.FlightID = flights.FlightID AND bookings.UserID = users.UserID AND users.Username = user;
+    SELECT bookings.BookingID, routes.From, routes.To, multiplebookings.FlightDate, flights.DepartTime, flights.FlightID, routes.Duration, multiplebookings.Class, multiplebookings.Passenger
+    FROM bookings, flights, multiplebookings, routes, users
+    WHERE multiplebookings.FlightID = flights.FlightID AND flights.RouteID = routes.RouteID AND bookings.BookingID = multiplebookings.BookingID AND bookings.UserID = users.UserID AND users.Username = user;
 END //
 DELIMITER ;
+
+# CALL MyTrips('Quinnow')
 
 DELIMITER //
 CREATE PROCEDURE ValidateBookingChange(IN user VARCHAR(45), IN bookingNumber VARCHAR(6), IN flightNumber VARCHAR(6), IN passengerName VARCHAR(100))
 BEGIN
     SELECT bookings.BookingID
-    FROM bookings, users
-    WHERE bookings.UserID = users.UserID AND users.Username = user AND bookings.BookingID = bookingNumber AND bookings.FlightID = flightNumber AND bookings.Passenger = passengerName AND DATEDIFF(bookings.FlightDate,CURDATE()) >= 2;
+    FROM bookings, multiplebookings, users
+    WHERE bookings.BookingID = multiplebookings.BookingID AND bookings.UserID = users.UserID AND users.Username = user AND bookings.BookingID = bookingNumber AND multiplebookings.FlightID = flightNumber AND multiplebookings.Passenger = passengerName AND DATEDIFF(multiplebookings.FlightDate, CURDATE()) >= 2;
 END //
 DELIMITER ;
 
+# CALL ValidateBookingChange('Quinnow', 'ABCDEF', 'AA2470', 'Roger McCubbin')
+
 DELIMITER //
-CREATE PROCEDURE UpdateBooking(IN bookingNumber VARCHAR(6), IN flightNumber VARCHAR(6), IN passengerName VARCHAR(100), IN fDate DATE, IN newBookingNumber VARCHAR(6), IN newFlightNumber VARCHAR(6), IN newDate DATE)
+CREATE PROCEDURE UpdateBooking(IN bookingNumber VARCHAR(6), IN flightNumber VARCHAR(6), IN passengerName VARCHAR(100), IN fDate DATE, IN newFlightNumber VARCHAR(6), IN newDate DATE)
 BEGIN
-    UPDATE bookings
-    SET BookingID = newBookingNumber, FlightID = newFlightNumber, FlightDate = newDate
-    WHERE bookings.BookingID = bookingNumber AND bookings.FlightID = flightNumber AND Passenger = passengerName AND FlightDate = fDate;
+    UPDATE multiplebookings
+    SET FlightID = newFlightNumber, FlightDate = newDate
+    WHERE BookingID = bookingNumber AND FlightID = flightNumber AND Passenger = passengerName AND FlightDate = fDate;
 END //
 DELIMITER ;
+
+# CALL UpdateBooking('ABCDEF', 'AA2470', 'Roger McCubbin', '2020-03-24', 'AA8623', '2020-04-30')
 
 DELIMITER //
 CREATE PROCEDURE DeleteBooking(IN bookingNumber VARCHAR(6), IN flightNumber VARCHAR(6), IN passengerName VARCHAR(100))
 BEGIN
-    DELETE FROM bookings
+    DELETE FROM multiplebookings
     WHERE BookingID = bookingNumber AND FlightID = flightNumber AND Passenger = passengerName;
+    
+    DELETE FROM bookings
+    WHERE BookingID = bookingNumber AND
+        NOT EXISTS(SELECT 1 FROM multiplebookings WHERE BookingID = bookingNumber);
 END //
 DELIMITER ;
+
+# CALL DeleteBooking('XXXAAA', 'AA2470', 'Roger McCubbin')
 
 DELIMITER //
 CREATE PROCEDURE GetDepartLocations()
 BEGIN
-    SELECT DISTINCT `From` FROM flights;
+    SELECT DISTINCT `From` FROM routes;
 END //
 DELIMITER ;
+
+# CALL GetDepartLocations()
 
 DELIMITER //
 CREATE PROCEDURE GetArrivalLocations()
 BEGIN
-    SELECT DISTINCT `To` FROM flights;
+    SELECT DISTINCT `To` FROM routes;
 END //
 DELIMITER ;
+
+# CALL GetArrivalLocations()
+
+DELIMITER //
+CREATE PROCEDURE GetTitles()
+BEGIN
+    SELECT * FROM titles;
+END //
+DELIMITER ;
+
+# CALL GetTitles()
+
+DELIMITER //
+CREATE PROCEDURE GetSecurityQuestions()
+BEGIN
+    SELECT * FROM securityquestions;
+END //
+DELIMITER ;
+
+# CALL GetSecurityQuestions()
 
 DELIMITER //
 CREATE PROCEDURE GetNews(IN count INT)
@@ -187,6 +273,8 @@ BEGIN
 END //
 DELIMITER ;
 
+# CALL GetNews(3)
+
 DELIMITER //
 CREATE PROCEDURE GetContactDetails()
 BEGIN
@@ -194,3 +282,5 @@ BEGIN
     FROM contactus;
 END //
 DELIMITER ;
+
+# CALL GetContactDetails()
--- Query11.sql
+++ Query11.sql
@@ -2,37 +2,28 @@
 
 -- 0 connections
 -- Should be one result AA2470
-SELECT FlightID
-FROM flights
-WHERE flights.From = 'ORD' AND flights.To = 'LAX';
+SELECT flights.FlightID, flights.DepartTime, routes.From, routes.To, routes.Duration
+FROM flights, routes
+WHERE flights.RouteID = routes.RouteID AND routes.From = 'ORD' AND routes.To = 'LAX';
 
 -- 1 connection
 -- Should be one result AA8901 -> AA6846
 SELECT f1.FlightID, f2.FlightID
-FROM flights AS f1 JOIN flights AS f2 ON (f1.To = f2.From AND TIMEDIFF(f2.DepartTime, f1.DepartTime + f1.Duration) > '01:00:00' AND TIMEDIFF(f2.DepartTime, f1.DepartTime + f1.Duration) < '5:00:00')
-WHERE f1.From = 'CID' AND f2.To = 'JFK'
+FROM (SELECT flights.FlightID, flights.DepartTime, routes.From, routes.To, routes.Duration FROM flights, routes WHERE flights.RouteID = routes.RouteID) AS f1
+JOIN (SELECT flights.FlightID, flights.DepartTime, routes.From, routes.To, routes.Duration FROM flights, routes WHERE flights.RouteID = routes.RouteID) AS f2
+ON (f1.To = f2.From AND TIMEDIFF(f2.DepartTime, ADDTIME(f1.DepartTime, f1.Duration)) > '01:00:00' AND TIMEDIFF(f2.DepartTime, ADDTIME(f1.DepartTime, f1.Duration)) < '5:00:00')
+WHERE f1.From = 'CID' AND f2.To = 'JFK';
 
 -- 2 connections
 -- Should be AA8901 -> AA6846 -> AA6881
 SELECT f1.FlightID, f2.FlightID, f3.FlightID
-FROM ...
-WHERE f1.From = 'CID' AND f2.To = 'MIA'
+FROM (SELECT flights.FlightID, flights.DepartTime, routes.From, routes.To, routes.Duration FROM flights, routes WHERE flights.RouteID = routes.RouteID) AS f1
+JOIN (SELECT flights.FlightID, flights.DepartTime, routes.From, routes.To, routes.Duration FROM flights, routes WHERE flights.RouteID = routes.RouteID) AS f2
+JOIN (SELECT flights.FlightID, flights.DepartTime, routes.From, routes.To, routes.Duration FROM flights, routes WHERE flights.RouteID = routes.RouteID) AS f3
+ON (f1.To = f2.From AND TIMEDIFF(f2.DepartTime, ADDTIME(f1.DepartTime, f1.Duration)) > '01:00:00' AND TIMEDIFF(f2.DepartTime, ADDTIME(f1.DepartTime, f1.Duration)) < '5:00:00' AND
+    f2.To = f3.From AND TIMEDIFF(f3.DepartTime, ADDTIME(f2.DepartTime, f2.Duration)) > '01:00:00' AND TIMEDIFF(f3.DepartTime, ADDTIME(f2.DepartTime, f2.Duration)) < '5:00:00')
+WHERE f1.From = 'CID' AND f3.To = 'MIA';
 
--- Then we will need a query that checks which flights happen before the returning ones
--- For example if an outgoing and incoming flight is on the same day, the query should show only options where outgoing flight happens before the returning one
--- Let's say that we want a round trip from ORD to DEN. We call the 0 connections procedure
-SELECT *
-FROM flights
-WHERE flights.From = 'ORD' AND flights.To = 'DEN';
--- We get 3 results: AA4594, AA5144, AA8623
--- On the website we would call it again from DEN to ORD for a returning flight
-SELECT *
-FROM flights
-WHERE flights.From = 'DEN' AND flights.To = 'ORD'; 
--- Results: AA3287
--- In conclusion, only flight AA4594 is before AA3287 which should be the result.
--- It's impossible to take other flights
-
--- Then we will a query that will combine all these options and display a final result
--- Yes I found a possible trip and here it is
--- No I did not find such a trip
+-- Then we will need a query that checks if a first flight happens before the returning one
+-- For example if an outgoing and incoming flight are on the same day, the query should only show options where outgoing flight happens before the returning one
+-- Maybe do that check on our server after user selects flights. Probably the easiest solution
